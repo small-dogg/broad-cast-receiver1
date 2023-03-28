89e7674d7268440a3df15e63ab1c78be8d97b002
small-dogg <sy920112@naver.com>
2023-03-28 10:22:49 +0900

    feat: Broad Cast Receiver 예제작성

---
+++ app/src/main/java/com/smalldogg/study/android/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.smalldogg.study.android.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val brIntent = Intent("com.smalldogg.study.android.testbr")
+            sendBroadcast(brIntent)
+        }
+    }
+}
